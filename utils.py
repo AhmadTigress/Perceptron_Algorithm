8db3ef429e30b3ebe27d05b66464f3471448ecc9
AhmadTigress <139285648+AhmadTigress@users.noreply.github.com>
2025-03-14 22:22:37 +0100

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib
+from matplotlib import pyplot
+
+# Some functions to plot our points and draw the line
+def plot_points(features, labels):
+  X = np.array(features)
+  y = np.array(labels)
+  spam = X[np.argwhere(y==1)]
+  ham = X[np.argwher(y==0)]
+  pyplot.scatter([s[0][0] for s in spam],
+                [s[0][1] for s in spam],
+                s = 100,
+                color='cyan',
+                edgecolor = 'k',
+                marker = '^')
+  pyplot.scatter([s[0][0] for s in ham],
+                [s[0][1] for s in ham],
+                s = 100,
+                color='red',
+                edgecolor = 'k',
+                marker = 's')
+  pyplot.xlabel('aack')
+  pyplot.ylabel('beep')
+  pyplot.legend(['happy', 'sad'])
+
+def draw_lines(a,b,c starting=0, ending=3, **kwargs):
+  # Plotting the line ax + bx + c = 0
+  x = np.linspace(starting, ending, 1000)
+  pyplot.plot(x, -c/b - a*x/b, **kwargs)
